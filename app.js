279a97a53d90c9fd2a0b7a171c02cb2c42054551
Hudhaifah <hudhaifahz@gmail.com>
2019-08-24 14:16:35 -0700

    allow CORS 2

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var port = process.env.PORT || 3000;
 const db = require('./queries')
 var app = express();
 
-app.configure(function () {
-    app.use(allowCrossDomain);
-});
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get("/", function (req, res) {
     res.send(JSON.stringify({Hello: port}));
@@ -17,17 +19,3 @@ app.get('/names', db.getNames);
 app.listen(port, function(){
     console.log("Example app listening on port!");
 });
-
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
